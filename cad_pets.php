85a3f4e3681a1d7793990fee40421b4fcd4d4d09
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-10-08 11:46:34 -0300

    conexao cadastro doacao

--- cad_pets.php
+++ cad_pets.php
@@ -1,7 +1,27 @@
 <?php
-//pegue as informações dos inputs da " tela_cad_pets.php "
-//este espaço é par realiazar os cadastros dos pets, 
+include "./class/Adocao.php";
 
-//*ainda n criado
-// include 'CLASSE DO banco';
+$nome = $_POST['nome']; 
+$especie = $_POST ['especie'];
+$raca = $_POST ['raca'];
+$sexo = $_POST ['sexo'];
+$porte = $_POST ['porte'];
+$castrado = $_POST ['castrado'];
+$idade_valor = $_POST ['idade_pet'];
+$idade_tipo= $_POST['idade'];
+$vacinado = $_POST ['vacinado'];
+$motivo_da_doacao = $_POST ['motivo'];
 
+
+$adoacao = new Adocao();
+$resultado = $adoacao->cadastro($nome, $especie, $raca, $sexo, $porte, $castrado, $idade_valor, $idade_tipo, $motivo_da_doacao, $vacinado);
+
+if($_SERVER['REQUEST_METHOD'] = 'POST' && isset($_POST['cadastro']) ){
+
+    header("location:tela_cad_pets.php");
+}
+else {
+
+        echo'erro na conexao POST para fazer o cadastro';
+}
+ 
