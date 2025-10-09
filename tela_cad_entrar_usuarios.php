821f3432bfb268730cc47e7ba44d9d57ef430fef
Brian-loop <brian.vegacz@gmail.com>
2025-10-09 15:17:50 -0300

    Chamando funções js

--- tela_cad_entrar_usuarios.php
+++ tela_cad_entrar_usuarios.php
@@ -3,6 +3,7 @@ include './template/header.php';
 ?>
 <section class="tela_cad_entrar">
     <main class="group_usuarios_inputs">
+        <!-- sessão de login de usúario -->
         <nav class="entrar_usuario">
             <div class="titulo_cad_usuario"><h3>Login</h3></div>
             <div class="input-group">
@@ -14,36 +15,68 @@ include './template/header.php';
                 <label for="meuInput">Senha</label>
             </div>
         </nav>
+        <!-- formulario de cadastro -->
         <form action="cad_entrar_usuario.php" method="POST">
             <nav class="cad_usuario">
                 <div class="titulo_cad_usuario"><h3>Cadastre-se</h3></div>
 
                 <input type="hidden" name="cadastro" value="1">
-
+                
+                <!-- nome -->
                 <div class="input-group">
-                    <input type="text" id="nome" name="nome"  required onblur="validaNome()">
+                    <input type="text" id="nome" name="nome" required onblur="validaNome()" oninput="validaNome()" aria-describedby="msgErroNome">
                     <label for="nome" id="labelNome">Digite seu nome...</label>
-                    <span id="msgErroNome"></span>
+                    <span id="msgErroNome" class="mensagem-erro" role="alert" aria-live="polite">
+                        <i class="bi bi-check-circle icone-ok"></i>
+                        <i class="bi bi-x-circle icone-erro"></i>
+                        <span id="textoErro"></span>
+                    </span>
                 </div>
+                <!-- email -->
                 <div class="input-group">
-                    <input type="text" id="meuInput" name="email" required>
-                    <label for="meuInput">E-mail</label>
-                </div>
+                    <input type="text" id="email" name="email" required onblur="validaEmail()" oninput="validaEmail()" aria-describedby="msgErroEmail">
+                    <label for="email" id="labelEmail">E-mail</label>
+                    <span id="msgErroEmail" class="mensagem-erro" role="alert" aria-live="polite">
+                        <i class="bi bi-check-circle icone-ok"></i>
+                        <i class="bi bi-x-circle icone-erro"></i>
+                        <span id="textoErroEmail"></span>
+                    </span>
+                </div>                            
+
+                <!-- telefone e CPF -->
                 <div class="juntos">
                     <div class="input-group">
-                        <input type="text" id="meuInput" name="telefone" required title="Formato: (XX) XXXX-XXXX ou (XX) XXXXX-XXXX">
-                        <label for="meuInput">Telefone</label>
+                        <input type="text" id="telefone" name="telefone" oninput="validaTelefone()" onblur="validaTelefone()" required maxlength="14">
+                        <label for="telefone" id="labelTelefone">Telefone</label>
+                        <span id="msgErroTelefone" class="mensagem-erro" role="alert" aria-live="polite">
+                            <i class="bi bi-check-circle icone-ok"></i>
+                            <i class="bi bi-x-circle icone-erro"></i>
+                            <span id="textoErroTelefone"></span>
+                        </span>
                     </div>
                     <div class="input-group">
-                        <input type="text" id="meuInput" name="cpf" required>
-                        <label for="meuInput">CPF</label>
+                        <input type="text" id="cpf" name="cpf" required maxlength="14" onblur="initCpfValidation()">
+                        <label for="cpf" id="labelCpf">CPF</label>
+                        <span id="msgErroCpf" class="mensagem-erro-cpf" role="alert" aria-live="polite">
+                            <i class="bi bi-check-circle icone-ok"></i>
+                            <i class="bi bi-x-circle icone-erro"></i>
+                            <span id="textoErroCpf"></span>
+                        </span>
                     </div>
                 </div>
-                
+                <!-- Endereço -->
                 <div class="input-group">
-                    <input type="text" id="meuInput" name="endereco" required>
-                    <label for="meuInput">Endereço</label>
+                    <input type="text" id="endereco" name="endereco" required onblur="validaEndereco()" oninput="validaEndereco()" aria-describedby="msgErroEndereco" maxlength="160">
+                    <label for="endereco" id="labelEndereco">Endereço</label>
+                    <span id="msgErroEndereco" class="mensagem-erro" role="alert" aria-live="polite">
+                        <i class="bi bi-check-circle icone-ok"></i>
+                        <i class="bi bi-x-circle icone-erro"></i>
+                        <span id="textoErroEndereco"></span>
+                    </span>
                 </div>
+
+                
+
                 <div class="input-group">
                     <input type="text" id="meuInput" name="cep" required>
                     <label for="meuInput">CEP</label>
