2fdc202d01f4c765e0eaaa2e03d89963b34d259e
Brian-loop <brian.vegacz@gmail.com>
2025-10-10 15:05:04 -0300

    Mudança na estrutura

--- tela_cad_entrar_usuarios.php
+++ tela_cad_entrar_usuarios.php
@@ -55,15 +55,38 @@ include './template/header.php';
                         </span>
                     </div>
                     <div class="input-group">
-                        <input type="text" id="cpf" name="cpf" required maxlength="14" onblur="initCpfValidation()">
+                        <input type="text" id="cpf" name="cpf" required maxlength="14" oninput="validaCpf()" onblur="validaCpf()">
                         <label for="cpf" id="labelCpf">CPF</label>
                         <span id="msgErroCpf" class="mensagem-erro-cpf" role="alert" aria-live="polite">
                             <i class="bi bi-check-circle icone-ok"></i>
                             <i class="bi bi-x-circle icone-erro"></i>
-                            <span id="textoErroCpf"></span>
+                        <span id="textoErroCpf"></span>
                         </span>
                     </div>
                 </div>
+
+                <!-- CEP e Numero-->
+                <div class="juntos2">
+                    <div class="input-group">
+                        <input type="text" class="cep_input" id="cep" name="cep" required aria-describedby="msgErroCep" maxlength="9" oninput="validarCep()" onblur="validarCep(); buscarEnderecoPorCep();">
+                        <label for="cep" id="labelCep">CEP</label>
+                        <span id="msgErroCep" class="mensagem-erro" role="alert" aria-live="polite">
+                            <i class="bi bi-check-circle icone-ok"></i>
+                            <i class="bi bi-x-circle icone-erro"></i>
+                            <span id="textoErroCep"></span>
+                        </span>
+                    </div>
+                    <div class="input-group">
+                        <input type="number" class="num_input" id="numero" name="numero" required maxlength="4" onblur="validaNumero()" oninput="validaNumero()">
+                        <label for="numero" id="labelNumero">Número</label>
+                        <span id="msgErroNumero" class="mensagem-erro-numero" role="alert" aria-live="polite">
+                            <i class="bi bi-check-circle icone-ok"></i>
+                            <i class="bi bi-x-circle icone-erro"></i>
+                            <span id="textoErroNumero"></span>
+                        </span>
+                    </div>
+                </div>
+
                 <!-- Endereço -->
                 <div class="input-group">
                     <input type="text" id="endereco" name="endereco" required onblur="validaEndereco()" oninput="validaEndereco()" aria-describedby="msgErroEndereco" maxlength="160">
@@ -75,21 +98,28 @@ include './template/header.php';
                     </span>
                 </div>
 
-                
+                <!-- Senha -->
+                <div class="input-group">
+                    <input type="password" id="senha" name="senha" required maxlength="160" oninput="validaSenha()" onblur="validaSenha()" aria-describedby="msgErroSenha">
+                    <label for="senha" id="labelSenha">Senha</label>
+                    <span id="msgErroSenha" class="mensagem-erro" role="alert" aria-live="polite">
+                    <i class="bi bi-check-circle icone-ok"></i>
+                    <i class="bi bi-x-circle icone-erro"></i>
+                    <span id="textoErroSenha"></span>
+                    </span>
+                </div>
 
+                <!-- Confirmar senha -->
                 <div class="input-group">
-                    <input type="text" id="meuInput" name="cep" required>
-                    <label for="meuInput">CEP</label>
+                    <input type="password" id="confir_senha" name="confir_senha" required maxlength="160" oninput="confirmarSenha()" onblur="confirmarSenha()" aria-describedby="msgErroConfirSenha">
+                    <label for="confir_senha" id="labelConfirSenha">Confirmar senha</label>
+                    <span id="msgErroConfirSenha" class="mensagem-erro" role="alert" aria-live="polite">
+                    <i class="bi bi-check-circle icone-ok"></i>
+                    <i class="bi bi-x-circle icone-erro"></i>
+                    <span id="textoErroConfir_senha"></span>
+                    </span>
                 </div>
-                <div class="input-group">
-                    <input type="password" id="meuInput" name="senha" required>
-                    <label for="meuInput">Senha</label>
-                </div>
-                <div class="input-group">
-                    <input type="password" id="meuInput" name="confirmar-senha" required>
-                    <label for="meuInput">Confirmar senha</label>
-                </div>
-                <button class="button" type="submit">Enviar</button>
+                <button type="submit" class="btn btn-primary"  onclick="return validarFormulario()">Cadastrar</button>
             </nav>
         </form>
     </main>
