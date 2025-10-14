8f67c8f98a8ea79ae7911529d88b313e5ec3c108
Brian-loop <brian.vegacz@gmail.com>
2025-10-14 11:50:15 -0300

    Criando estrutura

--- tela_config_perfil.php
+++ tela_config_perfil.php
@@ -6,8 +6,9 @@ include './template/header.php';
 ?>
 <section class="config_tela">
     <main class="container_confg">
+        <div class="imagem-configuracoes"></div>
         <div class="confg_menu">
-
+            
         </div>
         <nav class="itens_confg1">
 
