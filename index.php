c608b45cca71c4380922b6321717c310a7bfcd85
Brian-loop <brian.vegacz@gmail.com>
2025-10-15 00:49:37 -0300

    Estrutura do index

--- index.php
+++ index.php
@@ -3,8 +3,21 @@ include './template/header.php';
 ?>
 
 <section class="apresentacao">
-
+    <div class="group_text_apresentacao">
+        <section class="mobile-img"></section>
+        <div class="txt_apresentacao1">
+            <h3>Todo animalzinha precisa ser amado!</h3>
+            <p>Adotar um pet é um gesto de amor que muda duas vidas: a do animal e a de quem o acolhe. Ao adotar, damos uma segunda chance a um bichinho que muitas vezes já enfrentou o abandono e a solidão. Ver um animal resgatado ganhar confiança, brincar e demonstrar carinho aos poucos é uma das experiências mais gratificantes que existem. É como se ele soubesse que, finalmente, encontrou um lar seguro e um coração disposto a cuidar dele com amor.</p>
+        </div>
+        <div class="txt_apresentacao2">
+            <p>Além disso, adotar é um ato de consciência e empatia. Enquanto muitos animais esperam por um lar em abrigos ou nas ruas, a compra estimula um ciclo de reprodução e comércio que nem sempre garante o bem-estar dos bichinhos. Ao escolher a adoção, ajudamos a reduzir o abandono e fortalecemos uma cultura de cuidado e respeito pelos animais. E o melhor de tudo é que o amor que recebemos em troca é puro e verdadeiro — um laço de amizade e gratidão que transforma a rotina e preenche a vida com alegria.</p>
+        </div>            
+    </div>
 </section>
-<section class="apresentacao">
+<section class="title">
+        <h1>doaçao</h1>
+</section>
+<section class="group_cards">
+
 </section>
     
